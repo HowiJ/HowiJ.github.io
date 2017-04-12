9feedaebfe190be3f113a3d8f93f81e61399a0bf
Howard Jiang <howard.yunghao.jiang@gmail.com>
2017-04-11 22:51:09 -0700

    init

---
+++ motions.js
@@ -0,0 +1,134 @@
+(function () {
+  const pages = document.querySelectorAll('.FullPage');
+
+  window.onpopstate = function(e) {
+    router(window.location.pathname.substr(1));
+  };
+
+  let currentPage = parseInt(window.location.pathname.substr(1)) || 0;
+  if (currentPage > pages.length-1) { currentPage = pages.length-1; }
+  let switchMode = false;
+  window.history.pushState({}, '', `/${currentPage}` )
+
+  const transitionPage = document.querySelector('.TransitionPage');
+  transitionPage.style.display = 'none';
+
+  pages[currentPage].style.display = 'block';
+  pages[currentPage].style.opacity = 100;
+
+  const keyframes = [
+    [
+      document.getElementById('name'),
+      document.getElementById('title'),
+      document.getElementById('scroll'),
+    ],
+    [
+      document.getElementById('kf_about_titl'),
+      document.getElementById('kf_about_desc'),
+      document.getElementById('kf_about_recs'),
+    ]
+  ]
+  function fireKeyFrames(page) {
+    if (keyframes[page]) {
+      if (keyframes[page][0]) {
+        keyframes[page][0].classList.remove('dispNone');
+        keyframes[page][0].classList.add('animOne');
+      }
+      if (keyframes[page][1]) {
+        keyframes[page][1].classList.remove('dispNone');
+        keyframes[page][1].classList.add('animTwo');
+      }
+      if (keyframes[page][2]) {
+        keyframes[page][2].classList.remove('dispNone');
+        keyframes[page][2].classList.add('animThree');
+      }
+    }
+  }
+
+  for (let i in keyframes) {
+    for (let j in keyframes[i]) {
+      keyframes[i][j].classList.add('dispNone');
+    }
+  }
+  fireKeyFrames(currentPage);
+
+  function router(next) {
+    if (next < 0)             { next = 0; }
+    if (next >= pages.length) { next = pages.length-1; }
+    if (next === currentPage) { switchMode = false; return; }
+    document.getElementById('title_next').innerHTML = pages[next].dataset.page;
+
+
+    transitionPage.style.display = 'block';
+    const max = 150;
+    const middle = max/2;
+
+    let time = 0;
+    let subTime = 0;
+    let mode = true;
+
+
+    const inter = setInterval(() => {
+      /////////////////////////////////////////
+      time++;
+      // Last Page || first page || Done
+      if (time >= max) {
+        transitionPage.style.opacity = 0;
+        clearInterval(inter); 
+        switchMode = false; 
+
+        transitionPage.style.display = 'none';
+        fireKeyFrames(currentPage); 
+        return;
+      } 
+      /////////////////////////////////////////
+
+      /////////////////////////////////////////
+      subTime++;
+      if (mode && subTime < middle) {
+        // Fade In
+        transitionPage.style.opacity = (subTime/middle)*2;
+      }
+      if (!mode && subTime < middle) {
+        // Fade Out
+        transitionPage.style.opacity = (1-subTime/middle)*2;
+      }
+
+      if ((mode && subTime >= middle) || (!mode && subTime >= middle)) {
+        mode = !mode;
+        subTime = 0;
+
+
+        pages[currentPage].style.opacity = 0;
+        pages[currentPage].style.display = 'none';
+        currentPage = next;
+        pages[currentPage].style.display = 'block';
+        window.history.pushState({}, '', `/${currentPage}` )
+        pages[currentPage].style.opacity = 1;
+      }
+      
+    }, 1000/60);
+    return;
+  }
+
+  function scrollHandler(e) {
+    if (Math.abs(e.deltaX) > 1) { return; }
+
+    if (e.deltaY < 0) {
+      // Scroll Up
+      if (!switchMode) {
+        switchMode = true;
+        router(currentPage-1);
+      }
+    } else {
+      // Scroll Down
+      if (!switchMode){
+        switchMode = true;
+        router(currentPage+1);
+      }
+    }
+  }
+
+  window.addEventListener('mousewheel', scrollHandler);
+  window.addEventListener('DOMMouseScroll', scrollHandler);
+}())
