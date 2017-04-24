d550256080a3adbc4b362758d2bbfc8e71a72abe
Howard Jiang <howard.yunghao.jiang@gmail.com>
2017-04-24 14:57:47 -0700

    added ignorestrings to eslint max-len

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,9 @@ module.exports = {
         "es6": true
     },
     "rules": {
-        "linebreak-style": ["error", "windows"]
+        "linebreak-style": ["error", "windows"],
+        "max-len": ["error", {
+            "ignoreStrings": true
+        }]
     }
 };
